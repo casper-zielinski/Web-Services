712b8038e4f9ab0c2481e576c558a34b6748947e
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-03-16 13:02:30 +0100

    refactor: improve filter query construction

--- rest-extended/notes/filter.js
+++ rest-extended/notes/filter.js
@@ -1,20 +1,34 @@
 const getFilterQuery = (filter) => {
-  let filterQuery = "";
+  let filterQueryTracker = [];
   let filterQueryArray = [];
-  if (filter.title && filter.description) {
-    filterQuery += "WHERE title = ? AND description = ?";
-    filterQueryArray = [filter.title, filter.description];
-  } else if (filter.title) {
-    filterQuery += "WHERE title = ?";
-    filterQueryArray = [filter.title];
-  } else if (filter.description) {
-    filterQuery += "WHERE description = ?";
-    filterQueryArray = [filter.description];
+
+  if (filter.title) {
+    filterQueryTracker.push("title = ? ");
+    filterQueryArray.push(filter.title);
+  }
+  if (filter.description) {
+    filterQueryTracker.push(" description = ? ");
+    filterQueryArray.push(filter.description);
+  }
+  if (filter.limit) {
+    filterQueryTracker.push(" limit = ? ");
+    filterQueryArray.push(filter.limit);
   }
 
+  const filterQuery = () => {
+    if (filterQueryTracker.length === 0) {
+      return "";
+    } else
+      return "WHERE " + (filterQueryTracker.length > 1
+        ? filterQueryTracker.join("AND ")
+        : filterQueryTracker[0]);
+  };
+
+  const filterQueryString = filterQuery();
+  console.log(filterQueryString);
   return {
     filterQueryArray: filterQueryArray,
-    filterQuery: filterQuery,
+    filterQuery: filterQueryString,
   };
 };
 
